4c99f56dadbc3406094e0b72afc9f94497c95242
Xelonely <90315875+Xelonely@users.noreply.github.com>
2021-11-10 12:11:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chị à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao chị cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, Em trai à :3',
+    question:'Em từng nghĩ sẽ không yêu chị tới khi thấy nụ cười ấy <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Ví dụ như không may, em không phải là người hoàn hảo để dành cho chị. Chị giúp em một tay để trở thành người đó nhé?.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/khoi.xer190901' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
